3c6853f1d55f9c26211d5596e7e2fd92faef51eb
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-09-15 10:51:09 +0200

    Code cleaning

--- BattleDev1.java
+++ BattleDev1.java
@@ -1,8 +1,3 @@
-/*******
- * Read input from System.in
- * Use: System.out.println to ouput your result to STDOUT.
- * Use: System.err.println to ouput debugging information to STDERR.
- * ***/
 package com.isograd.exercise;
 import java.util.*;
 
@@ -10,11 +5,13 @@ public class BattleDev1 {
 
 
 public static void main( String[] argv ) throws Exception {
+
 		String  line;
         Scanner sc = new Scanner(System.in);
         int nbPersonnes = sc.nextInt();
         String[] couleur = new String[nbPersonnes];
         for (int i = 0; i < nbPersonnes; i++) {
+
             couleur[i] = sc.nextLine();
         }
     
@@ -25,9 +22,9 @@ public static void main( String[] argv ) throws Exception {
         int nbVert = 0;
         int nbViolet = 0;
         int nbBleu = 0;
-
         
         for (int i = 0; i < nbPersonnes; i++) {
+
             switch (couleur[i]) {
 
                 case "jaune": nbJaune++;
@@ -50,39 +47,44 @@ public static void main( String[] argv ) throws Exception {
 
                 case "bleu": nbBleu++;
 				break;
-                
 			} 	
         }
 
         int[] tableCouleur = {nbJaune, nbRouge, nbRose, nbOrange, nbVert, nbViolet, nbBleu};
-
 		int max = - 1;
 		String colorMax = "";
 		if (tableCouleur[0] > max) {
+
 			colorMax = "jaune";
 			max = tableCouleur[0];
 		}
 		if (tableCouleur[1] > max) {
+
 			colorMax = "rouge";
 			max = tableCouleur[1];
 		}
 		if (tableCouleur[2] > max) {
+
 			colorMax = "rose";
 			max = tableCouleur[2];
 		}
 		if (tableCouleur[3] > max) {
+
 			colorMax = "orange";
 			max = tableCouleur[3];
 		}
 		if (tableCouleur[4] > max) {
+
 			colorMax = "vert";
 			max = tableCouleur[4];
 		}
 		if (tableCouleur[5] > max) {
+
 			colorMax = "violet";
 			max = tableCouleur[5];
 		}
 		if (tableCouleur[6] > max) {
+
 			colorMax = "bleu";
 			max = tableCouleur[6];
 		}
@@ -90,38 +92,41 @@ public static void main( String[] argv ) throws Exception {
 		int max2 = - 1;
 		String colorMax2 = "";
 		if (tableCouleur[0] > max2 && "jaune" != colorMax) {
+
 			colorMax2 = "jaune";
 			max2 = tableCouleur[0];
 		}
 		if (tableCouleur[1] > max2 && "rouge" != colorMax) {
+
 			colorMax2 = "rouge";
 			max2 = tableCouleur[1];
 		}
 		if (tableCouleur[2] > max2 && "rose" != colorMax) {
+
 			colorMax2 = "rose";
 			max2 = tableCouleur[2];
 		}
 		if (tableCouleur[3] > max2 && "orange" != colorMax) {
+
 			colorMax2 = "orange";
 			max2 = tableCouleur[3];
 		}
 		if (tableCouleur[4] > max2 && "vert" != colorMax) {
+
 			colorMax2 = "vert";
 			max2 = tableCouleur[4];
 		}
 		if (tableCouleur[5] > max2 && "violet" != colorMax) {
+
 			colorMax2 = "violet";
 			max2 = tableCouleur[5];
 		}
 		if (tableCouleur[6] > max2 && "bleu" != colorMax) {
+
 			colorMax2 = "bleu";
 			max2 = tableCouleur[6];
 		}
-	
         Arrays.sort(tableCouleur);
-
         System.out.println(colorMax + " " + colorMax2);
-
-
     }
 }
--- BattleDev2.java
+++ BattleDev2.java
@@ -1,48 +1,44 @@
-/*******
- * Read input from System.in
- * Use: System.out.println to ouput your result to STDOUT.
- * Use: System.err.println to ouput debugging information to STDERR.
- * ***/
 package com.isograd.exercise;
 import java.util.*;
 
 public class BattleDev2 {
 
-    
-public static void main( String[] argv ) throws Exception {
+    public static void main( String[] argv ) throws Exception {
 
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int nbTirages = sc.nextInt(); 
-        
+        int nbTirages = sc.nextInt();
 
-        int nbMax = 0;  
-        
+        int nbMax = 0;
+
         int [] chiffres = new int [nbTirages];
         for (int i = 0; i < nbTirages; i++) {
-            chiffres[i] = sc.nextInt();   
+
+            chiffres[i] = sc.nextInt();
         }
 
         int count = 1;
 
         for (int i = 1; i < nbTirages; i++) {
-             // L
-            int D = chiffres[i - 1];
-           if( chiffres[i] == D ) {
-            count++;
-           } else {
 
-            nbMax = Math.max(nbMax,count);
-               D = chiffres[i];
-                count = 1;
-           }
-           if (i == nbTirages - 1) {
-               if (chiffres[i] == chiffres[i-1]) {
-                    // count++;
+            int D = chiffres[i - 1];
+            if( chiffres[i] == D ) {
+
+            count++;
+            } else {
+
+                nbMax = Math.max(nbMax,count);
+                    D = chiffres[i];
+                    count = 1;
+            }
+            if (i == nbTirages - 1) {
+
+                if (chiffres[i] == chiffres[i-1]) {
+
                     nbMax = Math.max(nbMax,count);
-               }
-           }
-        }   
-        System.out.println(nbMax);    
+                }
+            }
+        }
+        System.out.println(nbMax);
     }
 }
